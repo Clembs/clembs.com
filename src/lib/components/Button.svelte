536aed907ca9e3f133630cb4d6742dfbed9f2dcb
Clembs <clembs@clembs.com>
2023-05-22 13:14:33 +0200

    feat: update button styles

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	export let href = '';
 	export let style: 'primary' | 'secondary' = 'primary';
-	export let inline = false;
 	export let disabled = false;
-	export let background = 'var(--color-primary)';
-	export let color = 'white';
+	export let type: 'submit' | 'button' = 'button';
+	// export let background = 'var(--color-primary)';
+	// export let color = 'white';
 </script>
 
 {#if href && !disabled}
-	<a class="button {style}" class:inline {href}>
+	<a class="button {style}" {href}>
 		<slot name="icon" />
 		<slot />
 	</a>
 {:else}
-	<button on:click on:submit class="button {style}" class:inline {disabled}>
+	<button on:click on:submit {type} class="button {style}" {disabled}>
 		<slot name="icon" />
 		<slot />
 	</button>
@@ -35,32 +35,20 @@
 			opacity: 0.5;
 		}
 
-		&.inline {
-			text-align: center;
-			width: 100%;
-		}
-
-		appearance: none;
-		padding: 0.5rem 2.6rem;
-		font-weight: 500;
+		font-weight: 700;
+		padding: 0.8rem 1.6rem;
+		line-height: 1;
+		font-size: 1.2rem;
 		border: 1px solid black;
-		font-size: larger;
-		height: min-content;
-
-		border-radius: 1.5rem;
+		border-radius: 0.5rem;
 		user-select: none;
 		cursor: pointer;
-		display: block;
-		width: max-content;
+		text-align: center;
 		text-decoration: none;
-		transition: all 100ms ease-in;
+		display: inline-block;
+		position: relative;
+		// transition: all 100ms ease-in;
 		background-color: var(--button-bg);
 		color: var(--button-text);
-
-		&:hover {
-			border-bottom: 3px solid black;
-		}
-		&:active {
-		}
 	}
 </style>
