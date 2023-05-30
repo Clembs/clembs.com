ec0d26da9bc50a14c2740a7b59d1809ef46e6e66
Clembs <clembs@clembs.com>
2023-05-30 21:36:50 +0200

    feat: updated styles for buttons

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let href = '';
-	export let style: 'primary' | 'secondary' = 'primary';
+	export let style: 'filled' | 'outlined' | 'text' = 'filled';
 	export let disabled = false;
 	export let type: 'submit' | 'button' = 'button';
 	// export let background = 'var(--color-primary)';
@@ -8,47 +8,83 @@
 </script>
 
 {#if href && !disabled}
-	<a class="button {style}" {href}>
-		<slot name="icon" />
+	<a class="button {style}" {href} role="button">
 		<slot />
 	</a>
 {:else}
 	<button on:click on:submit {type} class="button {style}" {disabled}>
-		<slot name="icon" />
 		<slot />
 	</button>
 {/if}
 
 <style lang="scss">
 	.button {
-		&.primary {
-			--button-bg: black;
-			--button-text: white;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.7rem;
+
+		appearance: none;
+		position: relative;
+		user-select: none;
+		cursor: pointer;
+
+		font-size: 1rem;
+		font-family: inherit;
+		font-weight: 500;
+		text-align: center;
+		text-decoration: none;
+		max-width: max-content;
+
+		padding: 0.6rem 1.2rem;
+		border-radius: 99rem;
+
+		background-color: var(--_bg);
+		border: var(--_border);
+		color: var(--_text-color);
+
+		transition: background-color 150ms ease-in, max-width 150ms ease-in-out;
+
+		:global(svg) {
+			height: 18px;
+			width: auto;
 		}
-		&.secondary {
-			--button-bg: white;
-			--button-text: black;
+
+		&.filled {
+			--_bg: black;
+			--_border: 1px solid black;
+			--_text-color: white;
+			--_hover-bg: rgb(0 0 0 / 0.75);
+		}
+		&.outlined {
+			--_bg: transparent;
+			--_border: 1px solid black;
+			--_text-color: inherit;
+			--_hover-bg: rgb(0 0 0 / 0.2);
+		}
+		&.text {
+			--_bg: transparent;
+			--_border: none;
+			--_text-color: inherit;
+			--_hover-bg: rgb(0 0 0 / 0.2);
+		}
+
+		&:hover,
+		&:focus {
+			background-color: var(--_hover-bg);
+			// transition: background-color 150ms ease-out;
 		}
 
 		&:disabled {
 			cursor: not-allowed;
 			opacity: 0.5;
+			pointer-events: none;
 		}
+	}
 
-		font-weight: 700;
-		padding: 0.8rem 1.6rem;
-		line-height: 1;
-		font-size: 1.2rem;
-		border: 1px solid black;
-		border-radius: 0.5rem;
-		user-select: none;
-		cursor: pointer;
-		text-align: center;
-		text-decoration: none;
-		display: inline-block;
-		position: relative;
-		// transition: all 100ms ease-in;
-		background-color: var(--button-bg);
-		color: var(--button-text);
+	@media (prefers-reduced-motion: reduce) {
+		.button {
+			transition: none;
+		}
 	}
 </style>
