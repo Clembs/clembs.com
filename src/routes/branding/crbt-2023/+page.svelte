98df44b145273152d3061498e3e9913314055502
Clembs <clembs@clembs.com>
2023-06-07 21:00:45 +0200

    feat: WIP crbt-2023 branding page

--- src/routes/branding/crbt-2023/+page.svelte
+++ src/routes/branding/crbt-2023/+page.svelte
@@ -1,1 +1,52 @@
-hi
+<h2>
+	this page is not complete, and actually no other page is. this is a draft to look at what a
+	project page would look like, layout-wise.
+</h2>
+
+<p>
+	I started CRBT in 2019 as my very first coding experiment, and over the years it's become more
+	complete and defined, as a true multi-purpose Discord bot.
+	<br />
+	As the project evolved a lot the past year, its website needed a serious refresh to reflect it, and
+	what the future holds for it. Inspired by Material You, the new website features a clean and refined
+	design that works on all screen sizes and themes.
+</p>
+
+<h3>Interactive demo</h3>
+
+<p>
+	The multi-purpose Discord bot market being satured, CRBT's website needed to stand out. I opted
+	for a way to preview the bot by replicating the typical Discord experience!
+</p>
+
+<div class="iframe-wrapper">
+	<iframe title="CRBT website" src="https://crbt.app" />
+</div>
+
+<h3>Illustrations</h3>
+
+<p>
+	The new illustrations feature Microsoft's gorgeous Fluent Emojis and embrace the iconic Discord
+	embeds with different hues, representing CRBT's customization capabilities!
+</p>
+
+<img src="https://crbt.app/assets/welcome.svg" alt="image" />
+
+<style lang="scss">
+	.iframe-wrapper {
+		width: 100%;
+		height: 400px;
+		border-radius: 0.6rem;
+		border: 1px solid black;
+		overflow: hidden;
+		box-shadow: 0px 1px 0px 0px black;
+
+		iframe {
+			border: none;
+			width: calc(100% / 0.65);
+			height: calc(100% / 0.65);
+			transform-origin: 0 0;
+			transform: scale(0.65);
+		}
+	}
+</style>
