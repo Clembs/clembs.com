9bc6e9509e6df263a668b84abbe2ddb5cfd09fe4
Clembs <clembs@clembs.com>
2023-07-02 16:36:01 +0200

    feat: home face switcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
 	import Wordmark from './Wordmark.svelte';
 	import { onMount } from 'svelte';
 
+	let currentFace: 'logo' | 'irl' = 'irl';
+	let isAnimating = false;
+
 	onMount(() => {
 		const projects = document.querySelectorAll('.project');
 
@@ -24,13 +27,41 @@
 
 <main>
 	<div class="intro">
-		<div class="avatar">
-			<img
-				src="/assets/clément-election-2022.webp"
-				alt="Portrait of Clément smiling"
-				draggable="false"
-			/>
-		</div>
+		<button
+			class="avatar"
+			disabled={isAnimating}
+			on:click={() => {
+				if (isAnimating) return;
+				isAnimating = true;
+				setTimeout(() => (isAnimating = false), 500);
+				currentFace = currentFace === 'irl' ? 'logo' : 'irl';
+			}}
+		>
+			<!-- irl -->
+
+			<div
+				class="img-wrapper"
+				class:animating={isAnimating}
+				aria-hidden={currentFace !== 'irl'}
+				id="irl"
+			>
+				<img
+					src="/assets/clément-election-2022.webp"
+					alt="Portrait of Clément smiling"
+					draggable="false"
+				/>
+			</div>
+			<!-- logo -->
+
+			<div
+				class="img-wrapper"
+				class:animating={isAnimating}
+				aria-hidden={currentFace !== 'logo'}
+				id="logo"
+			>
+				<img src="/assets/logo-purplue.webp" alt="Clembs logo" draggable="false" />
+			</div>
+		</button>
 		<div class="text">
 			<h3>Nice to meet you, I'm</h3>
 			<Wordmark />
@@ -85,6 +116,34 @@
 </main>
 
 <style lang="scss">
+	@keyframes switchPfpFront {
+		0% {
+			z-index: 2;
+			transform: translate(0px, 0px) scale(1);
+		}
+		50% {
+			z-index: 0;
+			transform: translate(35px, 35px) scale(0.7);
+		}
+		100% {
+			transform: translate(5px, 5px) scale(1);
+		}
+	}
+
+	@keyframes switchPfpBack {
+		0% {
+			z-index: 0;
+			transform: translate(5px, 5px) scale(1);
+		}
+		50% {
+			z-index: 2;
+			transform: translate(-35px, -35px) scale(0.7);
+		}
+		100% {
+			transform: translate(0px, 0px) scale(1);
+		}
+	}
+
 	main {
 		padding: 1rem;
 		display: flex;
@@ -99,12 +158,61 @@
 		padding: 1rem 0;
 		margin: 0 auto;
 
-		.avatar img {
+		.avatar {
+			cursor: pointer;
+			appearance: none;
+			border: none;
+			position: relative;
+			background-color: transparent;
+			border-radius: 1rem;
+			padding: 0;
+			margin: 0;
 			width: 117px;
 			height: 117px;
 			border-radius: 999px;
-			border: 1px var(--color-on-background) solid;
-			box-shadow: 3px 3px 0px 0px var(--color-on-background);
+			box-shadow: none;
+
+			.img-wrapper {
+				position: absolute;
+				inset: 0;
+				z-index: 2;
+				transition: transform ease-in-out 100ms;
+
+				&.animating {
+					animation: switchPfpBack ease-in-out 500ms;
+				}
+
+				img {
+					width: 117px;
+					height: 117px;
+					border-radius: 999px;
+					border: 1px var(--color-on-background) solid;
+					// box-shadow: 3px 3px 0px 0px var(--color-on-background);
+				}
+
+				&::after {
+					border-radius: 999px;
+					content: '';
+					background-color: black;
+					inset: 0;
+					position: absolute;
+					opacity: 0;
+					transition: opacity 250ms ease-in-out;
+				}
+
+				&[aria-hidden='true'] {
+					&.animating {
+						animation: switchPfpFront ease-in-out 500ms;
+					}
+
+					z-index: 0;
+					transform: translate(5px, 5px);
+
+					&::after {
+						opacity: 1;
+					}
+				}
+			}
 		}
 
 		.text {
@@ -135,7 +243,7 @@
 		.project {
 			position: relative;
 			transition: transform ease-out 0.1s, box-shadow ease-out 0.1s;
-			border-radius: 0.5rem;
+			border-radius: 1rem;
 			border: 1px solid var(--color-on-background);
 			aspect-ratio: 16/9;
 			color: white;
@@ -143,6 +251,7 @@
 			place-items: center;
 			background-size: cover;
 			background-position: center;
+			overflow: hidden;
 
 			.year {
 				position: absolute;
@@ -151,16 +260,15 @@
 				color: var(--color-on-background);
 				background: var(--color-background);
 				border: 1px solid var(--color-on-background);
-				border-radius: 0.4rem;
-				font-size: 0.7rem;
+				border-radius: 0.5rem;
+				font-size: 0.8rem;
 				padding: 0.2rem 0.4rem;
-				font-weight: 400;
+				font-weight: 500;
 			}
 
 			img {
 				opacity: 0;
 				transition: opacity ease-in-out 300ms;
-				border-radius: 0.5rem;
 				object-position: center;
 				object-fit: cover;
 				width: 100%;
@@ -172,7 +280,7 @@
 			}
 
 			&:hover {
-				transform: translateY(-2px);
+				transform: translateY(-4px);
 				box-shadow: 0 4px 0 0 var(--color-on-background);
 			}
 		}
