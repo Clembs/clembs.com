ffe06b216169dbbe6c0d3d0ff9da777c463cd064
Clembs <clembs@clembs.com>
2023-07-02 23:00:16 +0200

    feat: add spline to technologies

--- src/lib/data/technologies.ts
+++ src/lib/data/technologies.ts
@@ -6,7 +6,7 @@ export enum TechnologyName {
 	Nextjs,
 	Illustrator,
 	XD,
-	GoogleSites
+	Spline,
 }
 
 export interface Technology {
@@ -19,41 +19,41 @@ export const technologies = [
 	{
 		id: TechnologyName.TypeScript,
 		name: 'TypeScript',
-		iconUrl: '/assets/technologies/typescript.svg'
+		iconUrl: '/assets/technologies/typescript.svg',
 	},
 	{
 		id: TechnologyName.Svelte,
 		name: 'Svelte',
-		iconUrl: '/assets/technologies/svelte.svg'
+		iconUrl: '/assets/technologies/svelte.svg',
 	},
 	{
 		id: TechnologyName.Figma,
 		name: 'Figma',
-		iconUrl: '/assets/technologies/figma.svg'
+		iconUrl: '/assets/technologies/figma.svg',
 	},
 	{
 		id: TechnologyName.TailwindCSS,
 		name: 'Tailwind CSS',
-		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/tailwindcss.svg'
+		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/tailwindcss.svg',
 	},
 	{
 		id: TechnologyName.Nextjs,
 		name: 'Next.js',
-		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/nextjs.svg'
+		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/nextjs.svg',
 	},
 	{
 		id: TechnologyName.Illustrator,
 		name: 'Adobe Illustrator',
-		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/adobeillustrator.svg'
+		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/adobeillustrator.svg',
 	},
 	{
 		id: TechnologyName.XD,
 		name: 'Adobe XD',
-		iconUrl: '/assets/technologies/xd.svg'
+		iconUrl: '/assets/technologies/xd.svg',
 	},
 	{
-		id: TechnologyName.GoogleSites,
-		name: 'Google Sites',
-		iconUrl: 'https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/google.svg'
-	}
+		id: TechnologyName.Spline,
+		name: 'Spline',
+		iconUrl: 'https://spline.design/images/icon_favicon32x32.png',
+	},
 ] satisfies Technology[];
