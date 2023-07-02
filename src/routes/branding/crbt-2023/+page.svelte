ad25161dc231cea82d7911dd047cb17fdd9da497
Clembs <clembs@clembs.com>
2023-07-02 23:11:39 +0200

    feat: update and finalize crbt-2023

--- src/routes/branding/crbt-2023/+page.svelte
+++ src/routes/branding/crbt-2023/+page.svelte
@@ -1,7 +1,4 @@
-<h2>
-	this page is not complete, and actually no other page is. this is a draft to look at what a
-	project page would look like, layout-wise.
-</h2>
+<h2>About</h2>
 
 <p>
 	I started CRBT in 2019 as my very first coding experiment, and over the years it's become more
@@ -12,41 +9,80 @@
 	design that works on all screen sizes and themes.
 </p>
 
-<h3>Interactive demo</h3>
+<img
+	class="stylized-element"
+	src="https://s.clembs.com/ZmKBURU.png"
+	alt="Screenshot of the CRBT website landing page"
+	loading="lazy"
+/>
+
+<div class="image-grid">
+	<img
+		class="stylized-element"
+		src="https://s.clembs.com/HrAKXuB.png"
+		alt="Screenshot of the CRBT website mobile landing page"
+		loading="lazy"
+	/>
+	<img
+		class="stylized-element"
+		src="https://s.clembs.com/jitjVXS.png"
+		alt="Screenshot of the CRBT website mobile landing page"
+		loading="lazy"
+	/>
+</div>
+
+<h2>Interactive demo</h2>
 
 <p>
 	The multi-purpose Discord bot market being satured, CRBT's website needed to stand out. I opted
 	for a way to preview the bot by replicating the typical Discord experience!
 </p>
 
-<div class="iframe-wrapper">
-	<iframe title="CRBT website" src="https://crbt.app" />
+<div class="image-grid">
+	<img
+		class="stylized-element"
+		src="https://s.clembs.com/ODcBCJR.png"
+		alt="Screenshot of the CRBT website interactive demo"
+		loading="lazy"
+	/>
+	<img
+		class="stylized-element"
+		src="https://s.clembs.com/Hbo4iTd.png"
+		alt="Screenshot of the CRBT website interactive demo"
+		loading="lazy"
+	/>
 </div>
 
-<h3>Illustrations</h3>
+<h2>Illustrations</h2>
 
 <p>
 	The new illustrations feature Microsoft's gorgeous Fluent Emojis and embrace the iconic Discord
 	embeds with different hues, representing CRBT's customization capabilities!
 </p>
 
-<img src="https://crbt.app/assets/welcome.svg" alt="image" />
+<div class="image-grid">
+	<img
+		class="stylized-element"
+		src="https://crbt.app/assets/welcome.svg"
+		alt="CRBT website illustration - Welcome & Farewell messages"
+		loading="lazy"
+	/>
+	<img
+		class="stylized-element"
+		src="https://crbt.app/assets/main-features.svg"
+		alt="CRBT website illustration - Polls, Giveaways, etc"
+		loading="lazy"
+	/>
+	<img
+		class="stylized-element"
+		src="https://crbt.app/assets/utilities.svg"
+		alt="CRBT website illustration - Utilities & Automation"
+		loading="lazy"
+	/>
+</div>
 
-<style lang="scss">
-	.iframe-wrapper {
-		width: 100%;
-		height: 400px;
-		border-radius: 0.6rem;
-		border: 1px solid var(--color-on-background);
-		overflow: hidden;
-		box-shadow: 0px 1px 0px 0px var(--color-on-background);
+<h2>Special thanks</h2>
 
-		iframe {
-			border: none;
-			width: calc(100% / 0.65);
-			height: calc(100% / 0.65);
-			transform-origin: 0 0;
-			transform: scale(0.65);
-		}
-	}
-</style>
+<p>
+	Thanks to <a href="https://paperdave.net">Dave</a> for helping with some of my CSS shenanigans.
+</p>
