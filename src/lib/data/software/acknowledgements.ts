0cfce4c64268512df745d4c7ed5ae47c58242756
Clembs <clembs@clembs.com>
2023-07-04 23:30:21 +0200

    feat(software): acknowledgements

---
+++ src/lib/data/software/acknowledgements.ts
@@ -0,0 +1,19 @@
+import type { Software } from '.';
+
+export const acknowledgements: Software = {
+	id: 'acknowledgements',
+	name: 'Acknowledgements',
+	category: 'Dependency list generator tool',
+	createdAt: new Date('2022-08-08'),
+	description: `Acknowledgments is a CLI tool that generates a JSON file with a list of all the local dependencies from package.json, as well as ones you add yourself, with exports to many file types (CSV, TOML, HTML, YAML and Markdown).`,
+	iconPath: 'https://i.imgur.com/MUEH8Un.png',
+	iconThumbnailPath: 'https://i.imgur.com/MUEH8Un.png',
+	links: {
+		projectUrl: 'https://npmjs.org/acknowledgements',
+		repoUrl: 'https://github.com/Clembs/acknowledgements/',
+	},
+	themeGradient: {
+		from: '#84E2B9',
+		to: '#5CAA8B',
+	},
+};
--- src/lib/data/software/index.ts
+++ src/lib/data/software/index.ts
@@ -1,3 +1,4 @@
+import { acknowledgements } from './acknowledgements';
 import { crbt } from './crbt';
 import { purplet } from './purplet';
 
@@ -21,6 +22,6 @@ export interface Software {
 	};
 }
 
-export const softwareData: Software[] = [crbt, purplet].sort(
+export const softwareData: Software[] = [crbt, purplet, acknowledgements].sort(
 	(a, b) => b.createdAt.getTime() - a.createdAt.getTime()
 );
