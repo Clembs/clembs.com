4d0a67213e12a309258040706d4ba98a2c02bd20
Clembs <clembs@clembs.com>
2023-07-05 12:55:40 +0200

    feat: mdsvex & shiki for markdown

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,26 @@
 import adapter from '@sveltejs/adapter-netlify';
+import { mdsvex } from 'mdsvex';
+import { getHighlighter } from 'shiki';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	extensions: ['.svelte', '.mdx'],
+	preprocess: [
+		mdsvex({
+			extensions: ['.mdx'],
+			highlight: {
+				async highlighter(code, lang, meta) {
+					const highlighter = await getHighlighter({
+						theme: 'github-dark-dimmed',
+					});
+
+					return highlighter.codeToHtml(code, { lang });
+				},
+			},
+		}),
+		preprocess(),
+	],
 
 	kit: {
 		adapter: adapter(),
