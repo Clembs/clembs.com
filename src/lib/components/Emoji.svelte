1873aba08a673e65679bb988017622c958d5843d
Clembs <clembs@clembs.com>
2023-07-10 22:03:10 +0200

    feat: emoji component

---
+++ src/lib/components/Emoji.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let src: string;
+	export let name: string;
+</script>
+
+<span class="emoji-wrapper">
+	<img aria-label=":{name}:" draggable="false" class="emoji" {src} alt=":{name}" title=":{name}:" />
+</span>
+
+<style lang="scss">
+	.emoji {
+		display: inline-block;
+		object-fit: contain;
+		cursor: pointer;
+		width: 1.375em;
+		height: 1.375em;
+		vertical-align: bottom;
+	}
+</style>
