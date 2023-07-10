5c056d0bb2683dcad3e24a49e497fdcd7ef20bcb
Clembs <clembs@clembs.com>
2023-07-10 22:02:59 +0200

    feat: don't enforce changing the activeTab in tabs

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -5,12 +5,11 @@
 	export let entries: { label: string; href?: string; id: string }[];
 	export let vertical = false;
 
-	export let activeTab = entries[0].id;
+	export let activeTab: string;
 
 	const dispatch = createEventDispatcher();
 
 	function changeTab(newEntryId: string) {
-		activeTab = newEntryId;
 		dispatch('change', newEntryId);
 	}
 </script>
@@ -26,12 +25,13 @@
 <style lang="scss">
 	ul {
 		display: flex;
-		border-radius: 1.25rem;
+		border-radius: 1.35rem;
 		border: 1px solid var(--color-on-background);
 		margin: 0;
 		padding: 0;
 		scrollbar-width: none;
 		overflow-y: scroll;
+		gap: 0.25rem;
 
 		:global(.chip) {
 			width: 100%;
