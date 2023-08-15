b82f275dabc657cb616f083af16c01e0714cfaa9
Clément Voisin <clembs@clembs.com>
2023-08-15 16:52:51 +0200

    fix: delete test file

--- i.js
+++
@@ -1,5 +0,0 @@
-console.log(new Date().toDateString());
-console.log(new Date().toISOString());
-console.log(new Date().toString());
-console.log(new Date().toLocaleTimeString());
-console.log(new Date().toTimeString());
