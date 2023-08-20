0d7f251db629683caea7389602ad60593b4c9804
Clément Voisin <clembs@clembs.com>
2023-08-20 04:15:07 +0200

    fix: secure like endpoint & use real data

--- src/routes/comments/api/[commentId]/like/+server.ts
+++ src/routes/comments/api/[commentId]/like/+server.ts
@@ -4,26 +4,32 @@ import { db } from '$lib/db';
 import { userCommentLikes } from '$lib/db/schema';
 import { eq } from 'drizzle-orm';
 
-export const PUT: RequestHandler = async ({ params }) => {
+export const PUT: RequestHandler = async ({ params, locals: { getUserData } }) => {
 	if (!params.commentId) {
 		throw error(400);
+	}
+
+	const currentUser = await getUserData();
+
+	if (!currentUser) {
+		throw error(401);
 	}
 
 	const commentLikes = await db.query.userCommentLikes.findMany({
 		where: ({ commentId }, { eq }) => eq(commentId, params.commentId),
 	});
 
-	let hasLiked = !!commentLikes.find(({ userId }) => userId === 'clembs');
+	let hasLiked = !!commentLikes.find(({ userId }) => userId === currentUser.id);
 
 	if (!hasLiked) {
 		await db.insert(userCommentLikes).values({
-			userId: 'clembs',
+			userId: currentUser.id,
 			commentId: params.commentId,
 		});
 
 		hasLiked = true;
 	} else {
-		await db.delete(userCommentLikes).where(eq(userCommentLikes.userId, 'clembs'));
+		await db.delete(userCommentLikes).where(eq(userCommentLikes.userId, currentUser.id));
 
 		hasLiked = false;
 	}
