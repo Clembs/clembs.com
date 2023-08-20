9b27771bb6c3de71c7fdaf2a1de107ef5703898a
Clément Voisin <clembs@clembs.com>
2023-08-20 04:11:54 +0200

    feat: rename SUPABASE_URL env var to POSTGRES_URL

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { SUPABASE_URL } from '$env/static/private';
+import { POSTGRES_URL } from '$env/static/private';
 import * as schema from './schema';
 
-const connectionString = SUPABASE_URL;
+const connectionString = POSTGRES_URL;
 const client = postgres(connectionString);
 
 export const db = drizzle(client, { schema });
