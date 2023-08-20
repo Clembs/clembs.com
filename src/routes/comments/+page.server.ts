e2d025efa436e3045503846b0c3a2869cec0fa70
Clément Voisin <clembs@clembs.com>
2023-08-20 04:16:17 +0200

    feat: load/get comment parent/children relations

--- src/routes/comments/+page.server.ts
+++ src/routes/comments/+page.server.ts
@@ -1,46 +1,62 @@
 import { db } from '$lib/db';
 import { comments } from '$lib/db/schema.js';
-import { snowflake } from '$lib/snowflake.js';
-import { error } from '@sveltejs/kit';
-import type { InferModel } from 'drizzle-orm';
+import { generateSnowflake } from '$lib/helpers/snowflake.js';
+import { fail } from '@sveltejs/kit';
+import { isNull, type InferModel } from 'drizzle-orm';
 
-export async function load() {
+export async function load({ locals }) {
+	const session = await locals.getSession();
+	const user = session?.user ?? null;
+
 	const comments = await db.query.comments.findMany({
 		with: {
 			author: true,
 			userLikes: true,
-			childComments: {
-				with: {
-					author: true,
-					userLikes: true,
-				},
-			},
+			childComments: true,
 		},
+		where: ({ parentId }) => isNull(parentId),
 	});
 
-	return { comments };
+	return { comments, user };
 }
 
 export type Comment = Awaited<ReturnType<typeof load>>['comments'][number];
 
 export const actions = {
-	post: async ({ request }) => {
+	post: async ({ request, locals }) => {
 		const formData = await request.formData();
 
 		if (!formData) {
-			throw error(400);
+			return fail(400);
 		}
 
+		const currentUser = await locals.getUserData();
 		const content = formData.get('content')?.toString();
+		const parentCommentId = formData.get('parent-comment')?.toString();
+
+		if (parentCommentId) {
+			const comment = await db.query.comments.findFirst({
+				where: ({ id }, { eq }) => eq(id, parentCommentId),
+			});
+
+			if (!comment) {
+				return fail(400, {
+					message: 'Invalid parent comment ID',
+				});
+			}
+		}
 
 		if (!content) {
-			throw error(400);
+			return fail(400, {
+				message: 'Missing content',
+			});
 		}
 
 		const input: InferModel<typeof comments, 'insert'> = {
-			id: snowflake.generate().toString(),
+			id: generateSnowflake(),
 			content: content,
-			userId: 'clembs',
+			userId: currentUser?.id,
+			parentId: parentCommentId,
 		};
 
 		await db.insert(comments).values(input);
