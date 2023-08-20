a20250c78ca0b17c2e395855416df56415af47c5
Clément Voisin <clembs@clembs.com>
2023-08-20 04:05:27 +0200

    feat: curve component

---
+++ src/lib/components/Curve.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let rotate = '0deg';
+</script>
+
+<svg
+	width="24"
+	height="24"
+	style="rotate:{rotate};"
+	viewBox="0 0 24 24"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path d="M24 0C10.7452 0 0 10.7452 0 24H2C2 11.8497 11.8497 2 24 2V0Z" fill="currentColor" />
+</svg>
