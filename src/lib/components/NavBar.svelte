99d3334a9ead8e7b43d25b7534fedcd837eb78e8
Clément Voisin <clembs@clembs.com>
2023-08-27 23:06:03 +0200

    feat: redesigned navbar w/ reorder, sounds, mobile

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import Clembs from '$lib/icons/Clembs.svelte';
+	import tap_01_wav from '../../sounds/tap_01.wav';
+	import disabled_wav from '../../sounds/disabled.wav';
+	import transition_up_wav from '../../sounds/transition_up.wav';
+	import transition_down_wav from '../../sounds/transition_down.wav';
+
+	let expandItems = false;
+	let showSettingsModal = false;
 
 	const navLinks = [
 		{
@@ -8,146 +16,233 @@
 			label: 'Home',
 		},
 		{
-			href: '/branding',
-			label: 'Design',
-		},
-		{
-			href: '/software',
-			label: 'Software',
+			href: '/projects',
+			label: 'Work',
 		},
 		{
 			href: '/comments',
 			label: $page.data.hasNameChange ? 'Clember' : 'Comments',
 		},
-		{
-			href: '/contact',
-			label: 'Contact',
-		},
+		// {
+		// 	href: '/contact',
+		// 	label: 'Contact',
+		// },
 	];
 
-	let firstNavEl: HTMLAnchorElement;
+	let scrolled = false;
+	let click_sound: HTMLAudioElement;
+	let disabled_sound: HTMLAudioElement;
+	let transition_up_sound: HTMLAudioElement;
+	let transition_down_sound: HTMLAudioElement;
 
-	onMount(() => (firstNavEl = document.querySelector('a[href="/"]')!));
+	function isScrolled() {
+		scrolled = (document?.scrollingElement?.scrollTop ?? 0) > 5;
+	}
+
+	onMount(() => {
+		isScrolled();
+
+		click_sound = new Audio(tap_01_wav);
+		disabled_sound = new Audio(disabled_wav);
+		transition_up_sound = new Audio(transition_up_wav);
+		transition_down_sound = new Audio(transition_down_wav);
+	});
 </script>
 
-<svelte:window
-	on:keydown={(ev) => {
-		if (ev.altKey && ev.key === 'n') {
-			ev.preventDefault();
-			firstNavEl.focus();
-		}
-	}}
-/>
+<svelte:window on:scroll={isScrolled} />
 
-<nav>
-	<div class="links">
-		{#each navLinks as link, i}
+<nav class:scrolled>
+	<div class="nav-contents">
+		<div class="main-elements">
 			<a
-				href={link.href}
-				aria-disabled={link.href === '/contact'}
-				class="nav-item"
-				class:selected={link.href === '/'
-					? $page.url.pathname === '/'
-					: $page.url.href.includes(link.href)}
-				aria-label={link.label}
+				href="/"
+				class="home"
+				on:click={() => ($page.url.pathname === '/' ? disabled_sound : click_sound).play()}
 			>
-				{link.label}
-				<div class="background" />
+				<Clembs />
 			</a>
-		{/each}
+
+			<button
+				on:click={() => {
+					expandItems = !expandItems;
+					(expandItems ? transition_up_sound : transition_down_sound).play();
+				}}
+				class="menu-button nav-item"
+				class:selected={expandItems}
+			>
+				{expandItems ? 'close' : 'menu'}
+			</button>
+		</div>
+
+		<div class="links" class:expand={expandItems}>
+			{#each navLinks as link, i}
+				{#if link.href}
+					<a
+						on:click={() => {
+							expandItems = false;
+							((link.href === '/' ? $page.url.pathname === '/' : $page.url.href.includes(link.href))
+								? disabled_sound
+								: click_sound
+							).play();
+						}}
+						href={link.href}
+						aria-disabled={link.href === '/contact'}
+						class="nav-item"
+						class:selected={!showSettingsModal &&
+							(link.href === '/' ? $page.url.pathname === '/' : $page.url.href.includes(link.href))}
+						aria-label={link.label}
+					>
+						{link.label}
+					</a>
+				{:else}
+					<button class="nav-item" class:selected={showSettingsModal} aria-label={link.label}>
+						{link.label}
+					</button>
+				{/if}
+			{/each}
+
+			{#if $page.data?.userData}
+				<a href="/account" class="nav-item"> Account </a>
+			{:else if $page.url.href === '/comments'}
+				<a href="/account" class="nav-item"> Login </a>
+			{/if}
+		</div>
 	</div>
 </nav>
 
 <style lang="scss">
 	nav {
-		display: flex;
-		align-items: center;
-		position: fixed;
-		height: 58px;
-		bottom: 0.75rem;
-		max-width: 100%;
-		border: 1px solid var(--color-on-background);
-		background-color: var(--color-background);
-		border-radius: 99rem;
-		padding: 0.25rem;
-		gap: 0.25rem;
-		box-shadow: 0px 2px 0px 0px var(--color-on-background);
-		transition: all linear 150ms;
+		position: sticky;
+		inset: 0;
 		z-index: 9;
-		overflow-y: scroll;
-		scrollbar-width: none;
 
-		&::-webkit-scrollbar {
-			display: none;
+		width: 100%;
+		gap: 1rem;
+
+		transition: background-color cubic-bezier(0.64, 0.005, 0.43, 1.01) 150ms;
+
+		&.scrolled {
+			background-color: var(--color-background);
+			border-bottom: 1px solid var(--color-outline);
 		}
 
-		.links {
+		.nav-contents {
 			display: flex;
-			gap: 0.25rem;
-			height: 100%;
-		}
-
-		.nav-item {
-			height: 100%;
-			display: flex;
-			position: relative;
-			color: var(--color-on-background);
-			padding: 0rem 0.75rem;
-			border-radius: 99rem;
-			text-decoration: none;
-			font-weight: 400;
-			font-variation-settings: 'wght' 400;
 			align-items: center;
-			white-space: nowrap;
+			justify-content: space-between;
+			padding: 1rem;
+			max-width: 1000px;
+			margin: 0 auto;
 
-			--transition-duration: 250ms;
-			--transition: var(--transition-duration) cubic-bezier(0, 0, 0.125, 1);
-			transition: all var(--transition);
-			user-select: none;
+			.main-elements {
+				display: flex;
+				justify-content: space-between;
+				width: 100%;
+				z-index: 10;
 
-			&[aria-disabled='true'] {
-				pointer-events: none;
-				text-decoration: line-through;
-				text-decoration-color: red;
-				color: var(--color-on-surface);
+				.home {
+					display: grid;
+					place-items: center;
+				}
+
+				.menu-button {
+					display: none;
+					border: 1px solid var(--color-outline);
+				}
 			}
 
-			.background {
-				position: absolute;
-				height: 100%;
-				width: 30px;
-				opacity: 0;
-				background: var(--main-gradient);
-				transition: width var(--transition);
-				border-radius: 999px;
-				z-index: -1;
-				left: 50%;
-				transform: translateX(-50%);
+			.links {
+				display: flex;
+				gap: 0.25rem;
+				align-items: center;
 			}
+			.nav-item {
+				display: flex;
+				position: relative;
+				color: var(--color-on-background);
+				padding: 0.5rem 1rem;
+				border-radius: 99rem;
+				text-decoration: none;
+				font-weight: 400;
+				font-variation-settings: 'wght' 400;
+				align-items: center;
+				white-space: nowrap;
+				background-color: inherit;
+				border: 1px solid transparent;
 
-			&.selected {
-				fill: white;
-				color: white;
-				font-weight: 600;
-				font-variation-settings: 'wght' 600;
-				background: var(--color-on-background);
-				padding: 0rem 1.25rem;
-			}
+				--transition-duration: 250ms;
+				--transition: var(--transition-duration) cubic-bezier(0, 0, 0.125, 1);
+				transition: font-variation-settings var(--transition), border var(--transition);
+				user-select: none;
 
-			&:hover:not(.selected) {
-				transition: all var(--transition);
-				background: var(--color-surface);
+				&[aria-disabled='true'] {
+					pointer-events: none;
+					text-decoration: line-through;
+					text-decoration-color: red;
+					color: var(--color-on-surface);
+				}
+
+				&.selected {
+					fill: white;
+					color: white;
+					font-weight: 600;
+					font-variation-settings: 'wght' 600;
+					background: var(--color-on-background);
+				}
+
+				&:hover:not(.selected) {
+					border: 1px solid var(--color-outline);
+					transition: all var(--transition);
+					font-variation-settings: 'wght' 500;
+				}
 			}
 		}
 	}
 
 	@media (max-width: 768px) {
-		nav {
-			.nav-item {
-				padding: 0 0.5rem;
+		nav .nav-contents {
+			.main-elements {
+				.menu-button {
+					display: block;
+				}
+			}
 
-				&.selected {
-					padding: 0 0.825rem;
+			.links {
+				background-color: var(--color-background);
+				position: absolute;
+				inset: 0;
+				translate: 0 -100%;
+				flex-direction: column;
+				width: 100%;
+				height: 100vh;
+				height: 100dvh;
+				padding: 1rem;
+				justify-content: center;
+				border-bottom: 1px solid var(--color-outline);
+				transition: translate 300ms cubic-bezier(0.64, 0.005, 0.43, 1.01);
+
+				.nav-item {
+					width: 100%;
+					// justify-content: center;
+					font-size: 2rem;
+
+					&.selected {
+						background-color: var(--color-background);
+						color: var(--color-on-background);
+						&::before {
+							content: '';
+							height: 0.5rem;
+							width: 0.5rem;
+							background-color: var(--color-on-background);
+							position: absolute;
+							left: 0rem;
+							border-radius: 50%;
+						}
+					}
+				}
+
+				&.expand {
+					translate: 0;
 				}
 			}
 		}
