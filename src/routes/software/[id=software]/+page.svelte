6d4aa029d21f36af4c1efcb45936e06507a2f1d6
Clément Voisin <clembs@clembs.com>
2023-08-27 23:14:43 +0200

    feat: use software grid in suggested software

--- src/routes/software/[id=software]/+page.svelte
+++ src/routes/software/[id=software]/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-	import '../../../styles/showcase.scss';
-	import IconMessageCircle from '@tabler/icons-svelte/dist/svelte/icons/IconMessageCircle.svelte';
+	// import '../../../styles/showcase.scss';
 	import { softwareData } from '$lib/data/software';
 	import Button from '$lib/components/Button.svelte';
 	import { onDestroy, onMount } from 'svelte';
-	import ProjectGrid from '$lib/components/Projects/ProjectGrid.svelte';
 	import { page } from '$app/stores';
 	import ShareButton from '$lib/components/ShareButton.svelte';
 	import MetaTags from '$lib/components/MetaTags.svelte';
 	import Comments from '../../comments/Comments.svelte';
 	import type { PageServerData } from './$types';
+	import SoftwareGrid from '$lib/components/Projects/SoftwareGrid.svelte';
 
 	let observer: IntersectionObserver;
 	let viewedImage = 0;
@@ -36,6 +35,10 @@
 	export let data: PageServerData;
 </script>
 
+<svelte:head>
+	<style src="../../../styles/showcase.scss"></style>
+</svelte:head>
+
 <MetaTags
 	pageName={data.name}
 	description="{data.category} •
@@ -51,35 +54,28 @@
 		<img src={data.iconPath} alt="{data.name} icon" draggable="false" />
 	</div>
 
-	<div class="about">
-		<h1 class="title">{data.name}</h1>
+	<div class="about-and-buttons">
+		<div class="about">
+			<h1 class="title">{data.name}</h1>
 
-		<span class="subtext">
-			{data.category} •
-			<time datetime={data.createdAt.toDateString()}>
-				{data.createdAt.toLocaleString('en-US', {
-					month: 'long',
-					day: 'numeric',
-					year: 'numeric',
-				})}
-			</time>
-		</span>
-	</div>
+			<span class="subtext">{data.category}</span>
+		</div>
 
-	<div class="buttons">
-		{#if data.links?.projectUrl}
-			<Button href={data.links?.projectUrl}>Check it out</Button>
-		{/if}
-		{#if data.links?.repoUrl}
-			<Button style={data.links?.projectUrl ? 'outlined' : 'filled'} href={data.links?.repoUrl}>
-				View source
-			</Button>
-		{/if}
-		<Button style="outlined" href="#comments">
-			<IconMessageCircle />
-			Comments
-		</Button>
-		<ShareButton url={$page.url.href} />
+		<div class="buttons">
+			{#if data.links?.projectUrl}
+				<Button href={data.links?.projectUrl}>Check it out</Button>
+			{/if}
+			{#if data.links?.repoUrl}
+				<Button style={data.links?.projectUrl ? 'outlined' : 'filled'} href={data.links?.repoUrl}>
+					View source
+				</Button>
+			{/if}
+			<!-- <Button style="outlined" href="#comments">
+				<IconMessageCircle />
+				Comments
+			</Button> -->
+			<ShareButton url={$page.url.href} />
+		</div>
 	</div>
 </header>
 
@@ -122,41 +118,58 @@
 <div class="suggested-apps">
 	<h3>Other apps by Clembs</h3>
 
-	<ProjectGrid projects={softwareData.filter(({ id }) => id !== data.id).slice(0, 3)} compact />
+	<SoftwareGrid projects={softwareData.filter(({ id }) => id !== data.id).slice(0, 3)} />
 </div>
+<!-- <time datetime={data.createdAt.toDateString()}>
+	{data.createdAt.toLocaleString('en-US', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric',
+	})}
+</time> -->
 
 <Comments projectId="{data.type}/{data.id}" comments={data.comments} userData={data.userData} />
 
 <style lang="scss">
 	header {
 		border-radius: 1rem 1rem 0 0;
-		border-bottom: 1px solid var(--color-on-background);
-		padding: 1.4rem;
+		border-bottom: 1px solid var(--color-on-surface);
+		padding: 1rem 1rem 2rem 1rem;
 		display: flex;
-		flex-direction: column;
+		gap: 2rem;
 		align-items: center;
-		gap: 0.5rem;
-		text-align: center;
 
 		.icon img {
-			border: 1px solid black;
-			height: 6rem;
-			width: 6rem;
-			border-radius: 1.5rem;
+			border: 1px solid var(--color-outline);
+			width: clamp(5rem, 8rem, 10rem);
+			height: auto;
+			border-radius: 2.5rem;
+			box-shadow: 0 2px 0 0 var(--color-on-background);
 		}
 
-		.about {
-			margin-bottom: 0.5rem;
+		.about-and-buttons {
+			.about {
+				display: flex;
+				flex-direction: column;
+				margin-bottom: 1rem;
+				gap: 0.25rem;
 
-			.title {
-				font-size: clamp(1.5rem, 5vw, 2rem);
-				margin: 0;
+				.title {
+					font-size: clamp(1.5rem, 5vw, 2rem);
+					margin: 0;
+				}
+
+				.subtext {
+					font-size: 0.9rem;
+					color: var(--color-on-surface);
+				}
 			}
-		}
-		.buttons {
-			display: flex;
-			flex-wrap: wrap;
-			gap: 0.5rem;
+			.buttons {
+				display: flex;
+				flex-wrap: wrap;
+				gap: 0.5rem;
+				align-items: center;
+			}
 		}
 	}
 
@@ -225,15 +238,22 @@
 
 	.suggested-apps {
 		border-top: 1px solid var(--color-on-background);
-		padding: 0.5rem 2rem;
+		padding: 0.5rem 1rem;
 	}
 
-	@media (max-width: 850px) {
-		.suggested-apps {
-			padding: 0.25rem 1.25rem;
+	@media (max-width: 768px) {
+		header {
+			flex-direction: column;
+			text-align: center;
+			gap: 1rem;
+
+			.buttons {
+				justify-content: center;
+			}
 		}
-		.buttons {
-			justify-content: center;
+
+		.suggested-apps {
+			padding: 0.25rem 1rem;
 		}
 	}
 </style>
