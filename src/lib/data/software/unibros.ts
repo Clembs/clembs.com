6a0d35bfc16a1d117ec779ddb893ac670494d4d8
Clément Voisin <clembs@clembs.com>
2023-08-29 01:49:27 +0200

    feat: WIP unibros software (?)

---
+++ src/lib/data/software/unibros.ts
@@ -0,0 +1,81 @@
+// ?
+
+// import type { Software } from '.';
+
+// export const unibros: Software = {
+// 	id: 'unibros',
+// 	name: 'New Super Mario UniBros.',
+// 	category: 'Level pack for a Mario Maker fan game',
+// 	createdAt: new Date('2020-05-16'),
+// 	description: `
+// A classic NSMB-like level pack for Super Mario UniMaker made by Nin★Star!<br />
+// <br />
+// - The game is in its demo state for now, which means anything can be modified or removed, and that more levels will eventually come later on.<br />
+// <br />
+// - There are five moons in each level which are collectibles similar to NSMB. Star Coins.<br />
+// <br />
+// - The game is most enjoyed by playing the levels in its order.<br />
+// <br />
+// - By the time Super Mario 2D Universe Studio releases I might make "UniBros. Deluxe" that fully utilize UniStudio’s features.<br />
+// <br />
+// - I am not affiliated with Nintendo, Nin★Star or UniMaker’s development. Is a 100% free and independent project.`,
+
+// 	links: {
+// 		downloadUrl: 'https://c.clembs.com/files/unibros-demo3.zip',
+// 		versionHistory: [
+// 			{
+// 				label: 'Demo 3',
+// 				url: 'https://c.clembs.com/files/unibros-demo3.zip',
+// 				createdAt: new Date('2021-01-17'),
+// 			},
+// 			{
+// 				label: 'Demo 2 - Patch 3',
+// 				url: 'https://c.clembs.com/files/unibros-demo2-patch3.zip',
+// 				createdAt: new Date('2020-11-23'),
+// 			},
+// 			{
+// 				label: 'Demo 2 - Patch 2',
+// 				url: 'https://c.clembs.com/files/unibros-demo2-patch2.zip',
+// 				createdAt: new Date('2020-06-18'),
+// 			},
+// 			{
+// 				label: 'Demo 2 - Patch 1',
+// 				url: 'https://c.clembs.com/files/unibros-demo2-patch1.zip',
+// 				createdAt: new Date('2020-06-17'),
+// 			},
+// 			{
+// 				label: 'Demo 2',
+// 				url: 'https://c.clembs.com/files/unibros-demo2.zip',
+// 				createdAt: new Date('2020-06-17'),
+// 			},
+// 			{
+// 				label: 'Demo 1 - Patch 1',
+// 				url: 'https://c.clembs.com/files/unibros-demo1-patch1.zip',
+// 				createdAt: new Date('2020-05-17'),
+// 			},
+// 			{
+// 				label: 'Demo 1',
+// 				url: 'https://c.clembs.com/files/unibros-demo1.zip',
+// 				createdAt: new Date('2020-05-16'),
+// 			},
+// 		],
+// 	},
+// 	platforms: ['windows'],
+// 	gallery: [
+// 		'https://i.imgur.com/sKqTY4i.png',
+// 		'https://i.imgur.com/yn2fOTR.png',
+// 		'https://i.imgur.com/frE39k4.png',
+// 		'https://i.imgur.com/WytU7lA.png',
+// 		'https://i.imgur.com/gRl6RPR.png',
+// 	],
+// 	themeGradient: {
+// 		from: '#01CD06',
+// 		to: '#01CD06',
+// 	},
+// 	credits: [
+// 		{
+// 			name: 'NinStar',
+// 			url: 'https://ninstars.blogspot.com/',
+// 		},
+// 	],
+// };
