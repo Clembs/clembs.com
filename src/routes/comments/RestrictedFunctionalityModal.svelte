70a68861f1cb1fca3583c4ab30ac5f2fceb4df34
Clément Voisin <clembs@clembs.com>
2023-09-05 19:55:05 +0200

    feat: add habile to restricted functional modal

--- src/routes/comments/RestrictedFunctionalityModal.svelte
+++ src/routes/comments/RestrictedFunctionalityModal.svelte
@@ -1,15 +1,29 @@
 <script>
 	import Modal from '$lib/components/Modal.svelte';
+	import HabileNeutral from '$lib/svg/HabileNeutral.svelte';
 
 	export let showModal = false;
 </script>
 
 <Modal bind:showModal>
-	<h1 slot="title">Your account has been blocked.</h1>
+	<header slot="title">
+		<HabileNeutral />
+
+		<h1>Your account has been blocked.</h1>
+	</header>
 
 	<p>
 		Because you have broken the rules, some of your account's functionality has been restricted.<br
 		/>
-		You can no longer use this account to like comments and post your own.
+		You can no longer use this account to vote on comments and post your own.
 	</p>
 </Modal>
+
+<style lang="scss">
+	header {
+		:global(svg) {
+			height: 4rem;
+			width: auto;
+		}
+	}
+</style>
