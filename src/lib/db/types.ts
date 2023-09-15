8644943b84e4d44b3fd834c717085b58469d4bf1
Clément Voisin <clembs@clembs.com>
2023-09-15 22:57:03 +0200

    feat: user mention types

--- src/lib/db/types.ts
+++ src/lib/db/types.ts
@@ -1,7 +1,10 @@
 import type { InferModel } from 'drizzle-orm';
 import type { comments, userCommentVote, users } from './schema';
+import type Mention from '../../routes/comments/Comment/Mention.svelte';
 
-export type User = InferModel<typeof users>;
+export type User = InferModel<typeof users> & {
+	comments?: Comment[];
+};
 
 export type UserCommentVote = InferModel<typeof userCommentVote>;
 
@@ -12,4 +15,7 @@ export type Comment = InferModel<typeof comments> & {
 	childComments?: Comment[] | null | undefined;
 	parentComment?: Comment | null | undefined;
 	score?: UserCommentVote[];
+	mentionedUsers?: (Mention & {
+		user: User;
+	})[];
 };
