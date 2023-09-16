f7dc74be2482f33cf79d32295e3a994f83bbdcad
Clément Voisin <clembs@clembs.com>
2023-09-17 01:43:25 +0200

    feat: comments bottom sheet component

---
+++ src/routes/comments/CommentsBottomSheet.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import BottomSheet from '$lib/components/BottomSheet.svelte';
+	import type { Comment, User } from '$lib/db/types';
+	import Comments from './Comments.svelte';
+
+	export let userData: User | null | undefined;
+	export let parentComment: Comment | null | undefined = null;
+	export let projectId: string | null = null;
+	export let comments: Comment[];
+	export let showLoginModal = false;
+	export let showSheet = false;
+</script>
+
+<BottomSheet bind:showSheet>
+	<Comments {userData} {comments} {parentComment} bind:showModal={showLoginModal} {projectId} />
+</BottomSheet>
