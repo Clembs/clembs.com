5c1dc1d4c198f5a1488b706628d5367001188b2f
Clément Voisin <clembs@clembs.com>
2023-10-24 00:42:37 +0200

    feat: "usage" endpoint to get user habile data

---
+++ src/routes/habile/api/usage/+server.ts
@@ -0,0 +1,26 @@
+import { HABILE_SECRET } from '$env/static/private';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { db } from '$lib/db';
+
+export const GET: RequestHandler = async ({ request }) => {
+	const secret = request.headers.get('Authorization');
+	const userId = request.headers.get('X-User-Id');
+
+	if (secret !== HABILE_SECRET) throw error(401, 'Invalid secret');
+	if (!userId) throw error(401, 'Invalid user id');
+
+	const userData = await db.query.users.findFirst({
+		where: ({ discordUserId }, { eq }) => eq(discordUserId, userId),
+		columns: {
+			username: true,
+			habileChatData: true,
+		},
+	});
+
+	if (!userData) throw error(404, 'User not found');
+
+	console.log({ userData, userId });
+
+	return json(userData.habileChatData);
+};
