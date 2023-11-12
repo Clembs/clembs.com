e3454bba20ffb20329e63859475021168bf6123d
Clément Voisin <clembs@clembs.com>
2023-11-13 00:33:40 +0100

    feat: more info in emoji tooltip

--- src/lib/components/Emoji.svelte
+++ src/lib/components/Emoji.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { emojiList } from '$lib/helpers/parseMentions';
 	import Tooltip from './Tooltip.svelte';
 
 	export let src: string;
@@ -6,11 +7,34 @@
 </script>
 
 <Tooltip transitionDelay={500}>
-	<span slot="tooltip-content">:{name}:</span>
+	<span slot="tooltip-content">
+		<div class="about">
+			<img aria-label=":{name}:" draggable="false" class="emoji big" {src} alt=":{name}:" />
+			<div class="about-text">
+				<div class="title">
+					:{name}:
+				</div>
+				{#if emojiList.includes(name)}
+					<div class="subtext">Type it in your comments to use.</div>
+				{/if}
+			</div>
+		</div>
+	</span>
 	<img aria-label=":{name}:" draggable="false" class="emoji" {src} alt=":{name}:" />
 </Tooltip>
 
 <style lang="scss">
+	.about {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+
+		.title {
+			font-size: 1rem;
+			font-weight: 500;
+		}
+	}
+
 	.emoji {
 		display: inline-block;
 		object-fit: contain;
@@ -19,5 +43,10 @@
 		vertical-align: bottom;
 		image-rendering: optimizequality;
 		vertical-align: bottom;
+
+		&.big {
+			width: 2.5rem;
+			height: 2.5rem;
+		}
 	}
 </style>
