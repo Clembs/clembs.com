2320d1a3485ff5cb6a968e190a0cd9dc3f3f03f5
Clément Voisin <clembs@clembs.com>
2023-11-13 00:40:21 +0100

    feat: add infobox on comments main feed page

--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import InfoBox from '$lib/components/InfoBox.svelte';
 	import type { PageData } from './$types';
 	import Comments from './Comments.svelte';
 
 	export let data: PageData;
 </script>
+
+<InfoBox type="note">
+	<span slot="title">Welcome to the Comments section!</span>
+	I created this so you can leave feedback, report bugs, ask questions and be chill.<br />
+	No spam or harmful content allowed. Use common sense.
+</InfoBox>
 
 <Comments comments={data.comments} userData={data.userData} />
