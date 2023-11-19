b814aabc3b361ed6bd7e9b10477fb15a41908105
Clément Voisin <clembs@clembs.com>
2023-11-19 21:42:58 +0100

    feat: update Social link schema & links

--- src/lib/data/socials.ts
+++ src/lib/data/socials.ts
@@ -1,28 +1,78 @@
+import Boosty from '$lib/icons/socials/boosty.svelte';
+import Discord from '$lib/icons/socials/discord.svelte';
+import Github from '$lib/icons/socials/github.svelte';
+import Instagram from '$lib/icons/socials/instagram.svelte';
+import KoFi from '$lib/icons/socials/ko-fi.svelte';
+import Mastodon from '$lib/icons/socials/mastodon.svelte';
+import Twitch from '$lib/icons/socials/twitch.svelte';
+import Youtube from '$lib/icons/socials/youtube.svelte';
+import IconMail from '@tabler/icons-svelte/dist/svelte/icons/IconMail.svelte';
+import IconMessageCircle from '@tabler/icons-svelte/dist/svelte/icons/IconMessageCircle.svelte';
+import IconDroplet from '@tabler/icons-svelte/dist/svelte/icons/IconDroplet.svelte';
+
 export enum SocialName {
 	Kofi = 'ko-fi',
+	Boosty = 'boosty',
 	// Fiverr = 'fiverr',
+	Email = 'email',
 	Discord = 'discord',
-	// Instagram = 'instagram',
+	Instagram = 'instagram',
+	Clembs = 'clembs',
 	GitHub = 'github',
-	// Twitch = 'twitch',
-	// Mastodon = 'mastodon',
+	Twitch = 'twitch',
+	Mastodon = 'mastodon',
 	YouTube = 'youtube',
 }
 
 export interface Social {
-	id: SocialName;
+	id?: SocialName;
 	name: string;
 	url: string;
-	background: string;
+	icon: any;
+	activity?: 'low' | 'medium' | 'high';
+	username?: string;
+	description?: string;
 }
 
-export const socials = [
+export const email: Social = {
+	id: SocialName.Email,
+	name: 'E-mail',
+	url: 'mailto://clembs@clembs.com',
+	icon: IconMail,
+	activity: 'high',
+	username: 'clembs@clembs.com',
+};
+
+export const donationLinks = [
 	{
 		id: SocialName.Kofi,
 		name: 'Ko-fi',
 		url: 'https://ko-fi.com/clembs',
-		background: '#00AFF1',
+		icon: KoFi,
+		description: 'Recommended. Supports one-time and monthly donations',
 	},
+	{
+		id: SocialName.Boosty,
+		name: 'Boosty',
+		url: 'https://boosty.to/clembs',
+		icon: Boosty,
+		description: 'If Ko-fi is not available in your country',
+	},
+	{
+		name: 'Water Aid',
+		url: 'https://wateraid.org/donate',
+		icon: IconDroplet,
+		description: 'Donate to those who are in real need of water',
+	},
+] satisfies Social[];
+
+export const socials = [
+	// {
+	// 	id: SocialName.Kofi,
+	// 	name: 'Ko-fi',
+	// 	url: 'https://ko-fi.com/clembs',
+	// 	icon: KoFi,
+	// },
 	// {
 	// 	id: SocialName.Fiverr,
 	// 	name: 'Fiverr',
@@ -33,36 +83,56 @@ export const socials = [
 		id: SocialName.Discord,
 		name: 'Discord',
 		url: 'https://discord.gg/6uNwP46',
-		background: '#5865F2',
+		icon: Discord,
+		activity: 'high',
+		username: "Habile's Lounge",
+	},
+	{
+		id: SocialName.GitHub,
+		name: 'GitHub',
+		url: 'https://github.com/Clembs',
+		icon: Github,
+		activity: 'high',
+		username: 'Clembs',
+	},
+	{
+		id: SocialName.Clembs,
+		name: 'Clembs Comments',
+		url: '/users/Clembs',
+		icon: IconMessageCircle,
+		activity: 'high',
+		username: 'Clembs',
 	},
 	{
 		id: SocialName.YouTube,
 		name: 'YouTube',
 		url: 'https://youtube.com/@ClembsV',
-		background: '#FF0000',
+		icon: Youtube,
+		activity: 'medium',
+		username: '@ClembsV',
 	},
-	// {
-	// 	id: SocialName.Twitch,
-	// 	name: 'Twitch',
-	// 	url: 'https://twitch.tv/clembs',
-	// 	background: '#9147FF',
-	// },
 	{
-		id: SocialName.GitHub,
-		name: 'GitHub',
-		url: 'https://github.com/clembs',
-		background: '#202020',
+		id: SocialName.Twitch,
+		name: 'Twitch',
+		url: 'https://twitch.tv/clembs',
+		icon: Twitch,
+		activity: 'low',
+		username: 'Clembs',
 	},
-	// {
-	// 	id: SocialName.Instagram,
-	// 	name: 'Instagram',
-	// 	url: 'https://instagram.com/clembs.v',
-	// 	background: '#FD0074',
-	// },
-	// {
-	// 	id: SocialName.Mastodon,
-	// 	name: 'Mastodon',
-	// 	url: 'https://mastodon.social/@clembs',
-	// 	background: '#6364FF',
-	// },
+	{
+		id: SocialName.Instagram,
+		name: 'Instagram',
+		url: 'https://instagram.com/clembs.v',
+		icon: Instagram,
+		activity: 'low',
+		username: '@clembs.v',
+	},
+	{
+		id: SocialName.Mastodon,
+		name: 'Mastodon',
+		url: 'https://mastodon.social/@clembs',
+		icon: Mastodon,
+		activity: 'low',
+		username: '@clembs@mastodon.social',
+	},
 ] satisfies Social[];
