cc4305cdc5e31e38e72de91875b6d2db2b5f77f9
Clément Voisin <clembs@clembs.com>
2023-12-10 15:47:37 +0100

    feat: getComment helper function

--- src/lib/helpers/getComments.ts
+++ src/lib/helpers/getComments.ts
@@ -2,17 +2,14 @@ import { db } from '$lib/db';
 import { comments } from '$lib/db/schema';
 import { and, eq, isNull, sql } from 'drizzle-orm';
 
-export async function getComments(options?: {
-	parentId?: string;
-	projectId?: string;
-	userId?: string;
-	onlyParentComments?: boolean;
-	includeParentComment?: boolean;
-	skipMentions?: boolean;
-	limit?: number;
-	page?: number;
-}) {
-	const results = await db.query.comments.findMany({
+export async function getComment(
+	commentId: string,
+	options?: {
+		includeParentComment?: boolean;
+		skipMentions?: boolean;
+	}
+) {
+	const results = await db.query.comments.findFirst({
 		with: {
 			author: {
 				columns: {
@@ -108,8 +105,149 @@ export async function getComments(options?: {
 				},
 			},
 		},
-		limit: options?.limit ?? 7,
-		offset: options?.page ? (options.page - 1) * (options.limit ?? 7) : undefined,
+		where: ({ id }, { eq }) => eq(id, commentId),
+	});
+
+	return results;
+}
+
+export async function getComments(options?: {
+	parentId?: string;
+	projectId?: string;
+	userId?: string;
+	onlyParentComments?: boolean;
+	includeParentComment?: boolean;
+	skipMentions?: boolean;
+}) {
+	const results = await db.query.comments.findMany({
+		with: {
+			author: {
+				columns: {
+					id: true,
+					username: true,
+					badges: true,
+				},
+			},
+			score: {
+				columns: {
+					type: true,
+					userId: true,
+				},
+			},
+			...(options?.includeParentComment
+				? {
+						parentComment: {
+							with: {
+								author: {
+									columns: {
+										id: true,
+										username: true,
+										badges: true,
+									},
+								},
+								score: {
+									columns: {
+										type: true,
+										userId: true,
+									},
+								},
+							},
+						},
+				  }
+				: {}),
+			...(options?.skipMentions
+				? {}
+				: {
+						mentionedUsers: {
+							with: {
+								user: {
+									columns: {
+										id: true,
+										username: true,
+										badges: true,
+									},
+								},
+							},
+						},
+				  }),
+			childComments: {
+				with: {
+					author: {
+						columns: {
+							id: true,
+							username: true,
+							badges: true,
+						},
+					},
+					score: {
+						columns: {
+							type: true,
+							userId: true,
+						},
+					},
+					childComments: {
+						columns: {
+							id: true,
+						},
+						with: {
+							author: {
+								columns: {
+									username: true,
+								},
+							},
+						},
+					},
+					...(options?.includeParentComment
+						? {
+								parentComment: {
+									with: {
+										author: {
+											columns: {
+												id: true,
+												username: true,
+												badges: true,
+											},
+										},
+										score: {
+											columns: {
+												type: true,
+												userId: true,
+											},
+										},
+										childComments: {
+											columns: {
+												id: true,
+											},
+											with: {
+												author: {
+													columns: {
+														username: true,
+													},
+												},
+											},
+										},
+									},
+								},
+						  }
+						: {}),
+					...(options?.skipMentions
+						? {}
+						: {
+								mentionedUsers: {
+									with: {
+										user: {
+											columns: {
+												id: true,
+												username: true,
+												badges: true,
+											},
+										},
+									},
+								},
+						  }),
+				},
+			},
+		},
 		orderBy: ({ createdAt }, { desc }) => desc(createdAt),
 		where: ({ parentId, projectId, userId }, { and, eq }) =>
 			and(
