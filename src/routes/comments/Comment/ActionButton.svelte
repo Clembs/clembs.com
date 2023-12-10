9f2dcfb250f33b3ddfbd94ada2f00c1ebbd35d26
Clément Voisin <clembs@clembs.com>
2023-12-10 15:34:56 +0100

    feat: action btn support for links

--- src/routes/comments/Comment/ActionButton.svelte
+++ src/routes/comments/Comment/ActionButton.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
 	import { LoaderIcon } from 'svelte-french-toast';
 
+	export let href = '';
 	export let loading = false;
 	export let big = false;
 </script>
 
-<button class:big on:click|stopPropagation {...$$restProps}>
-	{#if loading}
-		<LoaderIcon />
-	{:else}
+{#if href}
+	<a {href} class:big {...$$restProps}>
 		<slot />
-	{/if}
-</button>
+	</a>
+{:else}
+	<button class:big on:click|stopPropagation {...$$restProps}>
+		{#if loading}
+			<LoaderIcon />
+		{:else}
+			<slot />
+		{/if}
+	</button>
+{/if}
 
 <style lang="scss">
+	a,
 	button {
 		border: none;
 		color: var(--color-on-surface);
@@ -22,23 +30,24 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		padding: 0.5rem;
+		padding: 0.375rem;
 		cursor: pointer;
 		gap: 0.5rem;
 		font-size: 0.8rem;
 		max-width: max-content;
 		font-weight: 500;
 		border: 1px solid var(--color-outline);
-		min-width: 36px;
-		height: 36px;
+		min-width: 30px;
+		height: 30px;
+		text-decoration: none;
 
 		&:hover {
 			background-color: var(--color-surface);
 		}
 
 		:global(svg) {
-			width: 18px;
-			height: 18px;
+			width: 16px;
+			height: 16px;
 		}
 
 		&.big {
