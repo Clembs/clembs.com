8f0875a3dab08d46390ae9e0851ddddd0ef4f600
Clément Voisin <clembs@clembs.com>
2023-12-10 15:30:34 +0100

    refactor: shorter footer text

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,12 @@
 <footer>
-	&copy; 2020-{new Date().getFullYear()} Clembs. Made in France with love and
-	<a href="https://kit.svelte.dev" target="_blank" rel="noreferrer noopener"> SvelteKit</a>.
+	&copy; 2020-{new Date().getFullYear()} Clembs
 	<br />
 	<a href="mailto://clembs@clembs.com">clembs@clembs.com</a> • <a href="/contact">Contact me</a>
 </footer>
 
 <style lang="scss">
 	footer {
-		margin: 1rem;
+		margin-top: 1rem;
 		padding: 1rem;
 		width: 100%;
 		border-top: 1px solid var(--color-outline);
