7eff9e47f4d04fa7e7fdb9702767eb4f8397c1ef
Clément Voisin <clembs@clembs.com>
2023-12-10 15:35:42 +0100

    feat: remove "verified" badge

--- src/lib/helpers/badges.ts
+++ src/lib/helpers/badges.ts
@@ -1,24 +1,27 @@
 import type { UserBadge } from '$lib/db/types';
 import Clembs from '$lib/icons/badges/clembs.svelte';
 import Supporter from '$lib/icons/badges/supporter.svelte';
-import Verified from '$lib/icons/badges/verified.svelte';
+// import Verified from '$lib/icons/badges/verified.svelte';
 import Blocked from '$lib/icons/badges/blocked.svelte';
 
 export const badges: Record<
 	UserBadge,
 	{
+		color: string;
 		icon: any;
 		label: string;
 		description: string;
 	}
 > = {
 	BLOCKED: {
-		label: 'Blocked',
+		color: 'var(--color-error)',
+		label: 'Soft-banned',
 		icon: Blocked,
 		description: 'This user was blocked for breaking the Comments rules.',
 	},
 	CLEMBS: {
-		label: 'Clembs',
+		color: 'var(--color-primary)',
+		label: 'Admin',
 		icon: Clembs,
 		description: 'This is Clembs, the one and only.',
 	},
@@ -29,13 +32,14 @@ export const badges: Record<
 	// 	description: "The quirky cat robot who's helping with moderation.",
 	// },
 	SUPPORTER: {
+		color: 'var(--color-success)',
 		label: 'Supporter',
 		icon: Supporter,
 		description: 'This user supported me through Ko-fi or Boosty. Thanks!',
 	},
-	VERIFIED: {
-		label: 'Email verified',
-		icon: Verified,
-		description: 'This user has signed in and verified their email.',
-	},
+	// VERIFIED: {
+	// 	label: 'Email verified',
+	// 	icon: Verified,
+	// 	description: 'This user has signed in and verified their email.',
+	// },
 };
--- src/lib/helpers/rankBadges.ts
+++ src/lib/helpers/rankBadges.ts
@@ -1,11 +1,11 @@
 import type { User, UserBadge } from '$lib/db/types';
 
 export function rankBadges(badges: Exclude<User['badges'], null>): UserBadge[] {
-	const badgeImportance: UserBadge[] = ['BLOCKED', 'CLEMBS', 'SUPPORTER', 'VERIFIED'];
+	const badgeImportance: UserBadge[] = ['BLOCKED', 'CLEMBS', 'SUPPORTER'];
 
-	const sortedBadges = [...badges].sort(
-		(a, b) => badgeImportance.indexOf(a) - badgeImportance.indexOf(b)
-	);
+	const sortedBadges = [...badges]
+		.filter((badge) => badgeImportance.includes(badge))
+		.sort((a, b) => badgeImportance.indexOf(a) - badgeImportance.indexOf(b));
 
 	return sortedBadges;
 }
