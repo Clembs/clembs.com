8e2e713b3c939309f43204dcfb51ebaafa12f78b
Clément Voisin <clembs@clembs.com>
2023-12-10 15:30:10 +0100

    feat: add vite-plugin-tree-shaking to load icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { imagetools } from '@zerodevx/svelte-img/vite';
+import entryShakingPlugin from 'vite-plugin-entry-shaking';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), imagetools()],
+	plugins: [
+		sveltekit(),
+		imagetools({
+			profiles: {
+				run: new URLSearchParams('w=480;720;1024&format=avif;webp;png'),
+			},
+		}),
+		await entryShakingPlugin({
+			targets: ['@tabler/icons-svelte'],
+			extensions: ['svelte'],
+		}),
+	],
 };
 
 export default config;
