8bc5cc0cd35c97f76fd0d2b3e290d09de288e856
Clément Voisin <clembs@clembs.com>
2023-12-10 15:32:44 +0100

    feat: remove badge from gradient avatar

--- src/lib/components/GradientAvatar/GradientAvatar.svelte
+++ src/lib/components/GradientAvatar/GradientAvatar.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import { letterColors } from './letterColors';
 	import type { User } from '$lib/db/types';
-	import { badges } from '$lib/helpers/badges';
-	import { rankBadges } from '$lib/helpers/rankBadges';
-	import Tooltip from '../Tooltip.svelte';
 	import { tweened } from 'svelte/motion';
 	import { interpolate } from 'd3-interpolate';
 
 	export let user: null | Partial<User> = null;
-	export let showBadge = true;
 	export let showSilhouette = true;
 	export let size = '2.25rem';
 
@@ -42,19 +38,7 @@
 		letterColors[firstCharUsername]}; --color-b: {$avatarGradient.b ||
 		letterColors[lastCharUsername]}; --size: {size};"
 	title={username}
->
-	{#if user?.badges && showBadge}
-		{@const badge = badges[rankBadges(user.badges)[0]]}
-		<div class="avatar-badge" title={badge.label}>
-			<Tooltip>
-				<span slot="tooltip-content">{badge.label}</span>
-				<div aria-label="User top-most badge">
-					<svelte:component this={badge.icon} />
-				</div>
-			</Tooltip>
-		</div>
-	{/if}
-</div>
+/>
 
 <style lang="scss">
 	.avatar {
@@ -74,28 +58,6 @@
 			background-image: url('/assets/silhouette-black.webp');
 			background-size: cover;
 			opacity: 0.75;
-		}
-
-		// display: grid;
-		// place-items: center;
-		// color: white;
-		// font-weight: 600;
-		// text-shadow: 0 1px 0 0 var(--color-outline);
-		// background-size: cover;
-
-		&-badge {
-			position: absolute;
-			border: none;
-			bottom: -8px;
-			right: -8px;
-			width: 18px;
-			height: 18px;
-			z-index: 5;
-
-			:global(svg) {
-				width: 18px;
-				height: 18px;
-			}
 		}
 	}
 </style>
