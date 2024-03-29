8f320aadb8072ee17f609e01d255f33280e1332b
Clément Voisin <clembs@clembs.com>
2024-03-29 20:33:00 +0100

    feat: autoanimate comment list

--- src/routes/comments/Comment/CommentList.svelte
+++ src/routes/comments/Comment/CommentList.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import CommentComponent from './Comment.svelte';
 	import type { Comment } from '$lib/db/types';
+	import autoAnimate from '@formkit/auto-animate';
 
 	export let comments: Comment[];
 </script>
 
-<ul>
+<ul use:autoAnimate>
 	{#each comments as comment (comment.id)}
 		<CommentComponent {comment} />
 	{/each}
