8165a7cd1dc44aae1b174a3218c1cfb3b505377b
Clément Voisin <clembs@clembs.com>
2024-03-29 21:43:58 +0100

    refactor: simplify project grid structure

--- src/lib/components/Projects/ProjectGrid.svelte
+++ src/lib/components/Projects/ProjectGrid.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
 	import type { BlogPost } from '$lib/data/blog';
 	import type { Archive } from '$lib/data/archive';
-	import BlogPostCard from './BlogPost.svelte';
+	import BlogPostCard from './BlogArticleImage.svelte';
 	import ArchiveItem from './ArchiveItem.svelte';
 
 	export let projects: BlogPost[] | Archive[];
 	export let compact = false;
-	export let loaded = true;
 </script>
 
-<div class="projects" class:compact class:loaded>
+<div class="projects" class:compact>
 	{#each projects as project}
 		{#if 'title' in project}
-			<BlogPostCard {loaded} data={project} />
+			<BlogPostCard data={project} />
 		{:else}
-			<ArchiveItem {loaded} data={project} />
+			<ArchiveItem data={project} />
 		{/if}
 	{/each}
 </div>
@@ -25,23 +24,10 @@
 		width: 100%;
 		gap: 0.75rem;
 		padding-top: 2rem;
-		grid-template-columns: repeat(auto-fill, minmax(min(450px, 100%), 1fr));
-		transition: all cubic-bezier(0.64, 0.005, 0.43, 1.01) 200ms;
+		grid-template-columns: repeat(auto-fill, minmax(min(300px, 100%), 1fr));
 
 		&.compact {
 			grid-template-columns: repeat(auto-fit, minmax(min(200px, 100%), 1fr));
-		}
-
-		&:global(:not(.loaded) .project) {
-			opacity: 0;
-			transform: translateY(5rem);
-			transition-delay: calc(200ms * var(--delay));
-		}
-
-		:global(.project.show) {
-			opacity: 1;
-			transform: translateY(0);
-			transition-delay: 0s !important;
 		}
 	}
 </style>
