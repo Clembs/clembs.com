3cf7d0b725d40f6d491ee039242973bfdfdb84f2
Clément Voisin <clembs@clembs.com>
2024-12-22 18:50:13 +0100

    feat!: remove keyboard shortcuts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,13 @@
 <script lang="ts">
 	import '../styles/globals.scss';
-	import { page } from '$app/stores';
 	import NavBar from '$lib/components/NavBar.svelte';
 	import { Toaster } from 'svelte-french-toast';
 	import Footer from '$lib/components/Footer.svelte';
 	import type DebugMenu from './DebugMenu.svelte';
-	import ShortcutsModal from '../lib/components/ShortcutsModal/ShortcutsModal.svelte';
-	import { interpolate } from 'd3-interpolate';
-	import { tweened } from 'svelte/motion';
 	import ContentWrapper from '$lib/components/ContentWrapper.svelte';
-
-	const baseColors = {
-		from: '#643FFF',
-		to: '#31C0FF',
-	};
-
-	let colors = $page.data?.themeGradient ?? baseColors;
-
-	// this is literally too easy
-	const colorProgress = tweened<typeof $page.data.themeGradient>(colors, {
-		interpolate: interpolate,
-	});
 
 	// don't import this right away, it's pretty heavy
 	let debugMenu: typeof DebugMenu | null = null;
-
-	page.subscribe(({ error, data }) => {
-		if (error) {
-			colorProgress.set({
-				from: '#8a0000',
-				to: '#480000',
-			});
-		} else {
-			colorProgress.set(data?.themeGradient ?? baseColors);
-		}
-	});
 </script>
 
 <svelte:window
@@ -50,15 +23,9 @@
 	<svelte:component this={debugMenu} />
 {/if}
 
-<ShortcutsModal />
-
 <Toaster />
 
-<div
-	inert
-	class="background-piece"
-	style="--from: {$colorProgress?.from}; --to: {$colorProgress?.to}"
-/>
+<div inert class="background-piece" />
 
 <NavBar />
 
@@ -75,7 +42,7 @@
 		left: -10%;
 		width: 120%;
 		height: 50px;
-		background: linear-gradient(to right, var(--from), var(--to));
+		background: linear-gradient(to right, #643fff, #31c0ff);
 		filter: blur(50px);
 		z-index: -1;
 		position: fixed;
